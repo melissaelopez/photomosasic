1857f4f071b291d715007b2d987a9a1dff2adecb
unknown <zcl217@nyu.edu>
2017-12-12 01:04:40 -0500

    image storage after classification

--- photomosaic/models.py
+++ photomosaic/models.py
@@ -1,6 +1,13 @@
+import os
+import color_id
+
+import random, string
+
 from django.db import models
+from PIL import Image
 from django.utils import timezone
 
+from django.core.files.storage import Storage
 from django.core.files.storage import FileSystemStorage
 
 fs = FileSystemStorage(location='./media/photos')
@@ -16,3 +23,53 @@ class Photo(models.Model):
     title = models.CharField(max_length=255, blank=True)
     photo = models.ImageField(storage=fs)
     upload_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        #convert image to PIL object and classify it
+        pil_image = Image.open(self.photo)
+        color = color_id.get_classification('asdf', pil_image)
+        print color
+
+        #edit save file path to corresponding color folder
+        new_path = self.photo.path[:-(len(self.photo.name)+1)]
+        new_path =  os.path.join(new_path, color[0])
+        print new_path
+
+        #check if a folder exists for that color and create a new one if it doesn't
+        #race condition handling from:
+        #https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist
+        if not os.path.exists(new_path):
+            try:
+                os.makedirs(new_path)
+            except OSError as e:
+                if exception.errno != errno.EEXIST:
+                        raise
+
+        test_path = os.path.join(new_path, self.photo.name)
+
+        print test_path
+
+        
+        #check if image exists already
+        while os.path.exists(test_path):
+            #generate a random string
+            random_string = ''.join(random.choice(string.lowercase) for i in range(7))
+            random_string = '_' + random_string
+
+            #split extension from image name
+            split = os.path.splitext(self.photo.name)
+
+            #generate a new path with the random string appended to image name
+            test_path = os.path.join(new_path, split[0] + random_string + split[1])
+            print test_path
+
+        new_path = test_path
+        print 'new_path: ' + new_path
+        
+        #save the photo normally
+        super(Photo, self).save(*args, **kwargs)
+
+        #move the photo into the classified folder after saving it
+        os.rename(self.photo.path, new_path)
+        
